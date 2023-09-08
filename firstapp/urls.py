3a3c70da9dc83578e7203a174e19a6b2f699987d
Rahul Rudra <rahulrudra665@gmail.com>
2023-09-08 21:53:16 +0600

    delete done

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("home/", views.homepage),
     path("hometemplate/", views.homepagetemplate),
+    path("delete/<int:roll>", views.delete_user, name="delete_student"),
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.shortcuts import redirect
 from . import models
 
 
@@ -12,3 +12,10 @@ def homepagetemplate(request):
     student = models.Student.objects.all()
     print(student)
     return render(request, "home.html", {"data": student})
+
+
+def delete_user(request, roll):
+    std = models.Student.objects.get(pk=roll).delete()
+    print(std)
+    student = models.Student.objects.all()
+    return redirect("home")
