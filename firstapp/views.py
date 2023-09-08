b5a84db890b38185ba6183dd078c40489457fdcf
Rahul Rudra <rahulrudra665@gmail.com>
2023-09-08 16:56:42 +0600

    tables added

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from . import models
+
 
 # Create your views here.
 def homepage(request):
@@ -7,4 +9,6 @@ def homepage(request):
 
 
 def homepagetemplate(request):
-    return render(request, "home.html")
+    student = models.Student.objects.all()
+    print(student)
+    return render(request, "home.html", {"data": student})
